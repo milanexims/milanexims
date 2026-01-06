1c31ed09d2aae508327dcd09c390e3e93e05427e
milanexims <milanexims@gmail.com>
2026-01-06 14:45:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector(".hero-content").animate(
+  [
+    { opacity: 0, transform: "translateY(30px)" },
+    { opacity: 1, transform: "translateY(0)" }
+  ],
+  {
+    duration: 1200,
+    easing: "ease-out",
+    fill: "forwards"
+  }
+);
+
+
+
+
+

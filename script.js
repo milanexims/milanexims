778b5923e02eecbf98bbc4c2e76e645064e80c2d
milanexims <milanexims@gmail.com>
2026-01-06 23:21:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("mainNav");
+
+hamburger.addEventListener("click", function() {
+  nav.classList.toggle("open");
+});
+
+
+
+
+
+
+
+
